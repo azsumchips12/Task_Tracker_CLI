f9892b2f8e31abbeab14a8d35a56015e683f916b
Ангел Вангелов <angelthedark666@abv.bg>
2025-11-10 14:59:08 +0200

    starting using python oop

--- todocli.py
+++ todocli.py
@@ -1,47 +1,62 @@
 import typer
 from rich.console import Console
 from rich.table import Table
+from model import Todo
+from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
+
 
 console = Console()
 
 app = typer.Typer()
 
-@app.command(short_help="adds an item")
+@app.command(short_help='adds an item')
 def add(task: str, category: str):
-    typer.echo(f"Adding {task}, to {category}")
+    typer.echo(f"adding {task}, {category}")
+    todo = Todo(task, category)
+    insert_todo(todo)
+    show()
 
-@app.command(short_help="deletes an item")
+@app.command(short_help='deletes an item')
 def delete(position: int):
-    typer.echo(f"Deleting {position}")
+    typer.echo(f"deleting {position}")
+    # indices in UI begin at 1, but in database at 0
+    delete_todo(position-1)
+    show()
 
-@app.command(short_help="updates an item")
+@app.command(short_help='updates an item')
 def update(position: int, task: str = None, category: str = None):
-    typer.echo(f"Updating {position}")
+    typer.echo(f"updating {position}")
+    update_todo(position-1, task, category)
+    show()
 
-@app.command(short_help="mark a task as in progress")
-def mark_in_progress(task_id: int):
-    typer.echo(f"Marking {task_id} as in progress")
+@app.command(short_help='completes an item')
+def complete(position: int):
+    typer.echo(f"complete {position}")
+    complete_todo(position-1)
+    show()
 
-@app.command(short_help="mark task as done")
-def mark_done(task_id: int):
-    typer.echo(f"Marking {task_id} as done")
+@app.command(short_help='shows all the items')
+def show():
+    tasks = get_all_todos()
+    console.print("[bold magenta]Todos[/bold magenta]!", "💻")
 
-@app.command(short_help="list tasks that are not done")
-def list_todo():
-    typer.echo(f"Listing all tasks that are not done")
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Todo", min_width=20)
+    table.add_column("Category", min_width=12, justify="right")
+    table.add_column("Done", min_width=12, justify="right")
 
-@app.command(short_help="list tasks that in progress")
-def list_in_progress():
-    typer.echo(f"Listing all tasks that are in progress")
+    def get_category_color(category):
+        COLORS = {'Learn': 'cyan', 'YouTube': 'red', 'Sports': 'cyan', 'Study': 'green'}
+        if category in COLORS:
+            return COLORS[category]
+        return 'white'
 
-@app.command(short_help="list tasks that are done")
-def list_done():
-    typer.echo(f"Listing all tasks that are done")
-
-@app.command(short_help="list all tasks")
-def list_all():
-    typer.echo("Listing all tasks")
-
+    for idx, task in enumerate(tasks, start=1):
+        c = get_category_color(task.category)
+        is_done_str = '✅' if task.status == 2 else '❌'
+        table.add_row(str(idx), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
+    console.print(table)
 
 
 if __name__ == "__main__":
